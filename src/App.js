3a52dd28dc46842b2ba4256f83547fbe81dfb8a0
AleenaMad <aleena.madni@gmail.com>
2018-01-31 11:10:33 -0600

    todo lab

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import Header from './components/Header';
+import  MyRoutes from './config/routes';
+// import { Switch, Route } from 'react-router-dom';
+// import Home from './components/Home';
+// import TodosContainer from './containers/TodosContainer';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header/>
+      { MyRoutes }
+      </div>
+    );
+  }
+}
+
+export default App;
